004980c365a1e4a254f43b29568f248bbb6dcc78
gabaci72 <serianonim@gmail.com>
2025-01-20 16:28:31 +0100

    update

--- src/auth/google_auth.py
+++ src/auth/google_auth.py
@@ -1,17 +1,18 @@
+import os
+import json
 from google_auth_oauthlib.flow import InstalledAppFlow
-from google.auth.transport.requests import Request
-import google.auth
 
 SCOPES = ['https://www.googleapis.com/auth/drive.metadata.readonly']
+
 def get_google_auth_url(client_secrets_file):
-    flow = InstalledAppFlow.from_client_secrets_file(client_secrets_file, SCOPES)
+    client_secrets_json = json.loads(os.environ.get('GOOGLE_CLIENT_SECRET'))
+    flow = InstalledAppFlow.from_client_config(client_secrets_json, SCOPES)
     auth_url, _ = flow.authorization_url(prompt='consent')
     return auth_url
 
-def handle_google_callback(client_secrets_file, code):
-    flow = InstalledAppFlow.from_client_secrets_file(client_secrets_file, SCOPES)
+def handle_google_callback(code):
+    client_secrets_json = json.loads(os.environ.get('GOOGLE_CLIENT_SECRET'))
+    flow = InstalledAppFlow.from_client_config(client_secrets_json, SCOPES)
     flow.fetch_token(code=code)
     creds = flow.credentials
     return creds
-
-client_secrets_file = '/workspace/src/auth/client_secret.json'
