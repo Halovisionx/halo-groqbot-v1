895f4c9efa72736ddc00e3d93a4ecb76a5558480
gabaci72 <serianonim@gmail.com>
2025-01-20 14:46:57 +0100

    docker trying build

--- src/auth/google_auth.py
+++ src/auth/google_auth.py
@@ -2,15 +2,16 @@ from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
 import google.auth
 
-SCOPES = ['https://www.googleapis.com/auth/userinfo.profile', 'https://www.googleapis.com/auth/userinfo.email']
-
+SCOPES = ['https://www.googleapis.com/auth/drive.metadata.readonly']
 def get_google_auth_url(client_secrets_file):
     flow = InstalledAppFlow.from_client_secrets_file(client_secrets_file, SCOPES)
-    authorization_url, state = flow.authorization_url()
-    return authorization_url
+    auth_url, _ = flow.authorization_url(prompt='consent')
+    return auth_url
 
 def handle_google_callback(client_secrets_file, code):
     flow = InstalledAppFlow.from_client_secrets_file(client_secrets_file, SCOPES)
     flow.fetch_token(code=code)
     creds = flow.credentials
     return creds
+
+client_secrets_file = '/workspace/src/auth/client_secret.json'
